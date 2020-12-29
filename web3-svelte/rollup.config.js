a6ff2c41286ae01547f79b465bcf8baa9d80fdf8
Alexander Bai <vhurryharry@gmail.com>
2020-12-29 13:03:25 +0700

    Trying out Svelma Progress component; Missing Slider component

--- web3-svelte/rollup.config.js
+++ web3-svelte/rollup.config.js
@@ -48,12 +48,7 @@ export default {
         dev: !production,
       }
     }),
-    // we'll extract any component CSS out into
-    // a separate file - better for performance
-    // css({ output: 'public/build/bundle.css' }),
-    postcss({
-      extract: 'public/build/utils.css',
-    }),
+    postcss(),
 
     // If you have external dependencies installed from
     // npm, you'll most likely need these plugins. In
--- web3-svelte/src/components/ELTSwapWizard.svelte
+++ web3-svelte/src/components/ELTSwapWizard.svelte
@@ -1,85 +1,93 @@
 <script>
   import RangeSlider from "svelte-range-slider-pips";
+
+  import { Progress } from "svelma";
   // import { log } from "svelte-web3";
 </script>
 
 <style>
-  .unitEnclave {
+  .unit-enclave {
     height: 30;
     border-radius: 15;
+    background-color: "blue";
   }
 </style>
 
-<div class="ELTSwapWizard container">
-  <div class="header level">
-    <div class="unitEnclave">
+<div class="card">
+  <div class="card-header level">
+    <div class="unit-enclave column">
       <pre>0 HODL</pre>
     </div>
 
-    <div class="unitEnclave">
+    <div class="unit-enclave column">
       <pre>0 ELT</pre>
     </div>
 
-    <div class="unitEnclave">
-      <pre>0 ETH</pre>
+    <div class="unit-enclave column ">
+      <pre class="is-flex">0 ETH</pre>
 
-      <div class="unitEnclave">
+      <div class="unitEnclave ">
         <pre>Not Connected</pre>
         <pre class="connectionIndicator">&#11044;</pre>
       </div>
     </div>
   </div>
 
-  <div class="content container has-text-center">
-    <h3>ELT</h3>
-    <pre class="balance">100,000</pre>
+  <div class="card-content">
+    <div class="level ">
+      <div class="column">
+        <h3>ELT</h3>
+        <pre class="balance">100,000</pre>
+      </div>
 
-    <div class="sliderWrapper">
-      <RangeSlider />
+      <button
+        on:click={(evt) => {
+          console.log(evt);
+        }}>
+        Connect Wallet
+      </button>
 
-      <div class="colLeft">
+      <div class="column has-text-right">
+        <h3>HODL</h3>
+        <pre class="balance">0.0833</pre>
+      </div>
+    </div>
+
+    <div class="sliderWrapper level">
+      <div class="column">
         <pre>ELT Burn &#128293;</pre>
         <pre>66%</pre>
       </div>
 
-      <div class="colRight">
+      <div class="sliderWrapper level">
+        <RangeSlider />
+      </div>
+
+      <div class="column has-text-right">
         <pre>HODL Bonus;</pre>
         <pre>0.033%</pre>
       </div>
     </div>
-
-    <h3>HODL</h3>
-    <pre class="balance">0.0833</pre>
-
-    <button
-      on:click={(evt) => {
-        console.log(evt);
-      }}>
-      Connect Wallet
-    </button>
   </div>
 
-  <div class="footer">
-    <div class="colLeft">
-      <pre>Eltswap Progress</pre>
+  <div class="card-footer">
+    <div class="level">
+      <pre>Eltswap Progress: 133%</pre>
     </div>
 
     <div class="colRight">
-      <pre>30.1M ELT</pre>
-      <pre>133%</pre>
+      <pre>30,000,000 ELT</pre>
     </div>
   </div>
 
-  <div class="sliderWrapper">
-    <div class="absStatusIndicator">
-      <RangeSlider />
-    </div>
+  <div class="sliderWrapper level">
+    <Progress />
 
-    <div class="colLeft">
+    <div class="column">
       <pre>0 ELT</pre>
     </div>
 
-    <div class="colRight">
+    <div class="column">
       <pre>40M ELT</pre>
     </div>
   </div>
