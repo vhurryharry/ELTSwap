579038e854a91a5a6545dcddaff93e7e839b508c
Alexander Bai <vhurryharry@gmail.com>
2021-01-17 16:31:50 +0700

    Base styling

--- web3-svelte/src/components/ScreenOverlay/index.svelte
+++ web3-svelte/src/components/ScreenOverlay/index.svelte
@@ -1,4 +1,7 @@
 <script>
+  import Icon from "svelte-awesome";
+  import { times } from "svelte-awesome/icons";
+
   const closeOverlay = (evt) => {
     console.log(" /????? ", evt);
   };
@@ -7,20 +10,29 @@
 <style>
 </style>
 
-<div class="screen-overlay-wrapper is-flex is-12">
-  <div class="overlay-header">
-    <button class="button is-ghost" on:click={closeOverlay}>
-      <span class="icon is-small"> <i class="fas fa-times" /> </span>
-    </button>
+<div class="screen-overlay active is-flex is-12 has-text-center">
+  <div
+    class="screen-overlay-wrapper columns is-flex-direction-column is-justify-content-center p-5">
+    <div class="overlay-header column has-text-left">
+      <Icon data={times} class="close-knob" on:click={closeOverlay} />
+    </div>
+    <div class="overlay-content column">
+      <div class="block px-5 mb-5">
+        <p><code>Congratulations,</code> HODLER</p>
+        <p><code>Swap transaction:</code> SUCCESS ✅</p>
+      </div>
+
+      <div class="block px-5 mb-5">
+        <button class="button is-primary">
+          <span class="is-hidden-mobile"> Join the</span>
+          Telegram Community</button>
+      </div>
+      <div class="block px-5 mb-5">
+        <button class="button is-static" disabled>
+          Staking rewards dApp
+        </button>
+      </div>
+    </div>
+    <div class="overlay-footer " />
   </div>
-  <div class="overlay-content">
-    <p>Congratulations, HODLER</p>
-
-    <p>Swap transaction: SUCCESS ✅</p>
-
-    <button class="button is-primary"> Join the Telegram community </button>
-
-    <button class="button is-static" disabled> Staking rewards dApp </button>
-  </div>
-  <div class="overlay-footer" />
 </div>
