89e164cf74d53c2ba7c4933b03cf580b4d69f785
Alexander Bai <vhurryharry@gmail.com>
2021-01-28 19:54:25 +0700

    Added dynamic width

--- web3-svelte/src/components/SwapProgressBar/index.svelte
+++ web3-svelte/src/components/SwapProgressBar/index.svelte
@@ -23,14 +23,14 @@
 
   <div class="column is-12 elt-swap-progress-wrapper">
     <div id="swapProgress" class="is-flex is-12">
-      <span
-        id="swapProgressGradient"
-        style="--progress-bar-width: {$$props.getSwapProgress() || 33}%;" />
+      <span id="swapProgressGradient" />
       <span id="minSwapMark" />
       <span
         id="currentSwapMark"
         style="--curr-mark-left: {$$props.getSwapProgress()}%;">{$$props.getSwapProgress() > 10 ? $$props.eltInContract + ' ELT' : ''}</span>
-      <div id="coverProgressGradient" />
+      <div
+        id="coverProgressGradient"
+        style="--cover-progress-gradient-width: {99}%;" />
     </div>
   </div>
 </div>
