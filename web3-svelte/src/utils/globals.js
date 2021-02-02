2be924b97bd69fd97f8ad62c90125a33359b8dce
Alexander Bai <vhurryharry@gmail.com>
2021-02-02 01:25:24 +0000

    Updated swap contract.

--- web3-svelte/src/utils/globals.js
+++ web3-svelte/src/utils/globals.js
@@ -2,7 +2,7 @@ export const absMaxELT = Number(40 * 10 ** 6);
 export const maxELTToSwap = Number(100 * 10 ** 6);
 export const minELTToSwap = Number(10 ** 4);
 export const nilAccount = "0x0000000000000000000000000000000000000000";
-export const swapContractAddress = "0xfD440Ff3Bf3158F5CAf52E45269442938E9B8DeA";
+export const swapContractAddress = "0x3C34ecbB6e70f4E2b44d4FF012bB826AB14Cc36c";
 export const ELTTokenContractAddr = "0xa84a0b15d7c62684b71fecb5ea8efe0e5af1d11b";
 export const HODLTokenContractAddr = "0x5c85a93991671dc5886203e0048777a4fd219983";
 
